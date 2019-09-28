9b843d55e5dda4a6ba9efa8570829d9953af4342
Bernard Lucas <blucas1597@gmail.com>
2019-09-28 17:51:12 -0400

    traversing through nodes stable ver

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 var height = 10;
 var width = 12;
-var startCoord = [2,3];
-var endCoord = [9,9];
+var startCoord = [2,9];
+var endCoord = [2,1];
 var openSet = [];
 var closedSet = [];
 
@@ -21,7 +21,8 @@ function init(){
         newNode.h = Math.abs(x - endCoord[0]) + Math.abs(y - endCoord[1]);  //Calculate Manhattan distance
         newNode.f = newNode.g + newNode.h;                                  //Total f(n)
         newNode.coord = [x,y];
-        newNode.visited = false;
+        newNode.visited = true;
+        openSet.push(newNode);
       }else if(x == endCoord[0] && y == endCoord[1]){
         newNode.g = 1;
         newNode.h = 0;                                                      //End node has 0 h(n)
@@ -43,14 +44,72 @@ function init(){
 
 function search(){
   //insert iteration of search algo here
-  var newSet = [];
-
+  var currNode = openSet.shift();
+  closedSet.push(currNode);
   debugger;
+  grid[currNode.coord[1]][currNode.coord[0]].visited = true;
+
+  //check if node is on edge of grid
+  if(currNode.coord[1] > 0){            //check top
+    var top = grid[currNode.coord[1] - 1][currNode.coord[0]];
+    checkNode(currNode, top);
+  }
+
+  if(currNode.coord[1] < height - 1){   //check bottom
+    var bot = grid[currNode.coord[1] + 1][currNode.coord[0]];
+    checkNode(currNode, bot);
+  }
+
+  if(currNode.coord[0] > 0){            //check left
+    var left = grid[currNode.coord[1]][currNode.coord[0] - 1];
+    checkNode(currNode, left);
+  }
+
+  if(currNode.coord[0] < width - 1){    //check right
+    var right = grid[currNode.coord[1]][currNode.coord[0] + 1];
+    checkNode(currNode, right);
+  }
+
 
   paint();
   window.setTimeout(function(){
-    search();
+    //continue if openset is not empty
+    if(openSet.length > 0){
+      search();
+    }
   }, 500);
+}
+
+function checkNode(currNode, neighborNode){
+  if(!openSet.includes(neighborNode)){
+    //If not traversed yet, set parent to currNode, calculate f, & add to openSet
+    neighborNode.parent = currNode;
+    neighborNode.g += currNode.g;
+    neighborNode.f = neighborNode.g + neighborNode.h;
+    pushBubble(neighborNode);
+  }else{
+    if(neighborNode.g > currNode.g + 1){
+      //If already traversed but current g value is lower, reparent & recalculate f
+      neighborNode.parent = currNode;
+      neighborNode.g = currNode.g + 1;
+      neighborNode.f = neighborNode.g + neighborNode.h;
+    }
+  }
+}
+
+//helper function, inserts & bubblesorts new element into openSet based on f(n)
+function pushBubble(node){
+  openSet.push(node);
+  for(var i = openSet.length - 2 ; i > 0 ; i--){
+    if(openSet[i + 1].f < openSet[i].f){
+      //swap
+      var temp = openSet[i];
+      openSet[i] = openSet[i + 1];
+      openSet[i + 1] = temp;
+    }else{
+      break;
+    }
+  }
 }
 
 function paint(){
